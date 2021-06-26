3e5801419f10e4623ab474bca5e50b6e165b2b55
mbrady331 <mbrady331@gmail.com>
2021-06-26 13:56:43 -0500

    Create navigation menu for Home

---
+++ webapp/dictionary.py
@@ -0,0 +1,27 @@
+import justpy as jp
+import definition
+
+class Dictionary:
+    path = '/dictionary'
+
+    @classmethod
+    def serve(cls, req):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+        jp.Div(a=div, text="Instant English Dictionary", classes='text-4xl m-2')
+        jp.Div(a=div, text="Get the definition of any English word instantly.", classes='text-2xl')
+
+        input_div = jp.Div(a=div, classes=' grid grid-cols-2')
+
+        output_div = jp.Div(a=div, classes='m-2 p-2 text-lg border-2 border-gray-400 h-40')
+        input_box = jp.Input(a=input_div, placeholder='Type in a word here...', outputdiv = output_div,
+                             classes='m-2, bg-white border-2 border-gray-200 rounded w-64 '
+                                     'focus:bg-white focus:outline-none focus:border-blue-500 py-2 px-4')
+        input_box.on('input', cls.get_definition)
+
+        return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = definition.Definition(term=widget.value).get()
+        widget.outputdiv.text = " ".join(defined)
--- webapp/home.py
+++ webapp/home.py
@@ -3,9 +3,38 @@ import justpy as jp
 class Home:
     path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+
+        layout = jp.QLayout(a= wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="leftDrawerOpen",
+                            side='left', bordered=True)
+
+        scroller = jp.QScrollArea(a=drawer, classes='fit')
+        qlist = jp.QList(a=scroller)
+
+        a_classes = 'p-2 m-2 text-lg text-blue-400 hover:text-blue-700'
+
+        jp.A(a=qlist, text='Home', href='/', classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.A(a=qlist, text='About', href='/about', classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.A(a=qlist, text='Dictionary', href='/dictionary', classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.QBtn(a= toolbar, dense=True, flat=True, round=True, icon='menu',
+                click= cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant Dictionary')
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes='bg-gray-200 h-screen p-4')
         jp.Div(a=div, text="This is the Home Page", classes='text-4xl m-2')
         jp.Div(a=div, text="""
         I'm baby disrupt occaecat migas stumptown truffaut meditation scenester deserunt gluten-free shaman
@@ -16,4 +45,9 @@ class Home:
 
         return wp
 
-
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value == True:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
